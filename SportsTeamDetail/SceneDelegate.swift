41629f7f92b1f12c3706a706bb29490fd40796eb
Mauricio Vazquez <maurovz@gmail.com>
2022-09-18 16:10:27 +0200

    Set HomeViewController as root navigator, custom back color

--- SportsTeamDetail/SceneDelegate.swift
+++ SportsTeamDetail/SceneDelegate.swift
@@ -1,9 +1,19 @@
 import UIKit
+
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
   var window: UIWindow?
 
+  private var navigationController: UINavigationController {
+    let navigationController = UINavigationController()
+    navigationController.setViewControllers([HomeViewController()], animated: false)
+    return navigationController
+  }
+
   func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-    guard let _ = (scene as? UIWindowScene) else { return }
+    guard let scene = (scene as? UIWindowScene) else { return }
+
+    window = UIWindow(windowScene: scene)
+    window?.rootViewController = navigationController
+    window?.makeKeyAndVisible()
   }
 }
-
